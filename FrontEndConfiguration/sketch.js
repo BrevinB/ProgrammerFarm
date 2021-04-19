b67cd419d8ad420c87a2a62a48b90fffb46c4e8b
Brevin Blalock <30970021+BrevinB@users.noreply.github.com>
2021-04-19 16:01:33 -0400

    Add files via upload

---
+++ FrontEndConfiguration/sketch.js
@@ -0,0 +1,265 @@
+var cnv;
+var hay = [];
+var ans = [];
+var hayBaleImg, bg, pigImg, index, pig;
+var pigClicked = false;
+var hayClicked = false;
+var pigR, pigL, pigU, pigD, hayR, hayL, hayU, hayD = false;
+var level1 = false, level2 = false, level3 = false, level4 = false;
+var level1Check = false, level2Check = false; level3Check = false, level4Check = false;
+var total = 0;
+let pigSound;
+let backgroundSound;
+let haySound;
+
+function preload() {
+  soundFormats('mp3');
+  hayBaleImg = loadImage("Images/HayBale.png");
+  pigImg = loadImage("Images/Pig.png");
+  bg = loadImage("Images/Grass.png");
+  pigSound = loadSound('sounds/pig.mp3');
+  backgroundSound = loadSound('sounds/backgroundSound.mp3');
+  haySound = loadSound('sounds/hay.mp3');
+
+
+
+  generateQAndA();  
+  generateLevelTwoQAndA();
+  generateLevelThreeQAndA();
+  generateLevelFourQAndA();
+
+}
+
+
+function setup() {
+  backgroundSound.setVolume(.1);
+  pigSound.setVolume(.2);
+  haySound.setVolume(.2);
+  openNav();
+  cnv = createCanvas(600, 600);
+  cnv.position(375, 100);
+  //resize images
+  hayBaleImg.resize(80,90);  
+  pigImg.resize(80,80);
+  document.getElementById("ScoreBoard").innerHTML = "Score : " + total;
+
+
+  pig = new Pig(0,0);
+  
+} 
+
+
+function draw() {
+
+  background(bg);
+  //Setup grid interface
+  for (var x = 0; x < width; x += width / 7) {
+		for (var y = 0; y < height; y += height / 7) {
+
+			stroke(0);
+			strokeWeight(1);
+			line(x, 0, x, height);
+      line(0, y, width, y);
+      
+		}
+  }
+
+  
+  
+  if(level1 == true) {
+    
+    level1Check = true;
+    newLevel();
+    levelOne();
+    displayQ();
+    level1 = false;
+       
+  } else if(level2 == true) {
+    
+    level2Check = true;
+    newLevel();
+    levelTwo();
+    displayQ2();
+    level2 = false;
+        
+  } else if(level3 == true) {
+
+    level3Check = true;
+    newLevel();
+    levelThree();
+    displayQ3();
+    level3 = false;
+  
+  } else if(level4 == true) {
+
+    level4Check = true;
+    newLevel();
+    levelFour();
+    displayQ4();
+    level4 = false;
+  
+   } 
+
+
+  //setup option slots and starting position
+  let greenBlocks = color(0,128,0);
+  fill(greenBlocks);
+  //Top left corner
+  rect(0,-5,85,90);
+
+  //bottom right corner 
+  rect(515,515,85,90);
+
+  //top right corner
+  rect(515,-5,85,90);
+
+  //bottom left corner
+  rect(0,515,85,90);
+
+  //Middle
+  rect(0,172,85,85);
+  
+  //pig at starting position
+  pig.display();
+
+  //display haybales
+  for(i = 0; i < hay.length; i++) {
+
+    hay[i].display();
+   
+  }
+
+  for(i = 0; i < ans.length; i++) {
+
+    ans[i].display();
+
+  }
+
+
+
+}
+
+
+//check to see if there is a haybale where the mouse clicked and if there is perform the clicked funcion
+function mousePressed() {
+
+  for(let i = 0; i < hay.length; i++) {
+    if(pigClicked) {
+      break;
+    }
+
+    if(hay[i].clicked(mouseX, mouseY)) {
+
+      index = i;
+      hayClicked = true;
+      break;
+
+    }
+  }
+
+  if(hayClicked) {
+    
+  } else if(pig.clicked(mouseX, mouseY)) {
+      pigClicked = true;
+  }
+
+}
+
+
+//Move the haybales and set them to new location
+function keyPressed() {
+
+  if(keyCode == LEFT_ARROW && keyIsPressed) {
+
+    if(hayClicked) {
+      hayL = true;
+      haySound.play();
+      hay[index].column -= 85;
+      hay[index].checkBorder();
+      hay[index].checkHayBaleColission();
+      hayL = false;
+    }
+
+    if(pigClicked) {
+      pigL = true;
+      pig.column -= 85;
+      pig.checkBorder();
+      pig.checkHayBale();
+      pig.answerCheck();
+      pigL = false;
+      
+    }
+
+  }
+
+  if(keyCode == RIGHT_ARROW && keyIsPressed) {
+
+    if(hayClicked) {
+      hayR = true;
+      haySound.play();
+      hay[index].column += 85;
+      hay[index].checkBorder();
+      hay[index].checkHayBaleColission();
+      hayR = false;
+    }
+
+    if(pigClicked) {
+      pigR = true;
+      pig.column += 85;
+      pig.checkBorder();
+      pig.checkHayBale();
+      pig.answerCheck();
+      pigR = false;
+    }
+
+  }
+
+  if(keyCode == UP_ARROW && keyIsPressed) {
+
+    if(hayClicked) {
+      hayU = true;
+      haySound.play();
+      hay[index].row -= 85;
+      hay[index].checkBorder();
+      hay[index].checkHayBaleColission();
+      hayU = false;
+    }
+
+    if(pigClicked) {
+      pigU = true;
+      pig.row -= 85;
+      pig.checkBorder();
+      pig.checkHayBale();
+      pig.answerCheck();
+      pigU = false;
+    }
+  }
+
+  if(keyCode == DOWN_ARROW && keyIsPressed) {
+
+    if(hayClicked) {
+      hayD = true;
+      haySound.play();
+      hay[index].row += 85;
+      hay[index].checkBorder();
+      hay[index].checkHayBaleColission();
+      hayD = false;
+    }
+
+    if(pigClicked) {
+      pigD = true;
+      pig.row += 85;
+      pig.checkBorder();
+      pig.checkHayBale();
+      pig.answerCheck();
+      pigD = false;
+    }
+  }
+
+  if(keyCode == ENTER && keyIsPressed) {
+    for(let i = 0; i < hay.length; i++) {
+      hay[i].entered();
+    }
+    pig.entered();
+  }
+}
---
+++ Functionality/QuestionsAndAnswers4.js
@@ -0,0 +1,213 @@
+var cA; //to store correct answer info
+var answer4;
+var q;
+var answers4 = [];
+var questions4 = [];
+
+function generateLevelFourQAndA() {
+
+    questions4 = loadStrings("Text_Files/level4_questions.txt");
+    answers4 = loadStrings("Text_Files/level4_answers.txt");
+
+  for (let i = 0; i < questions4.length; i++)
+  {
+      answers4[i] = answers4[i].replace(/[\r]+/gm, "");
+      questions4[i] = questions4[i].replace(/[\r]+/gm, "");
+  }
+}
+
+function displayQ4() {
+    
+    var question = questions4[Math.floor(Math.random()*questions4.length)];
+
+    q = createElement('h1', question); //choose one random question
+
+    answer4 = correctA4(question); //get correct answer based on question displayed
+    print(answer4);
+    displayA(answer4); //pass correct answer into displayA()
+
+    q.position(375,5);
+}
+
+//display answers in their corresponding boxes
+function displayA(correctA) {
+    var aArray = [];
+
+    for (let i = 0; i < answers4.length; i++)
+    {
+        aArray[i] = answers4[i];
+    }
+
+    var m; //index of correct answer in 'aArray' array later on in function
+
+    for (let i = 0; i < aArray.length; i++)
+    {
+        if (aArray[i] == correctA)
+        {
+            m = i;
+            break;
+        }
+    }
+
+    //var a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11;
+    var y_coordinates = [290, 115, 635, 634];
+    //var answers = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11];
+
+    var x,y; //stores x and y coordinate
+    var i; //a random index from y_coordinates
+    var j = 0; //loop control for positioning at 4 answers
+    var n; //a random index from answers
+
+    while (y_coordinates.length != 0) {
+        
+        i = Math.floor(Math.random()*y_coordinates.length);
+        n = Math.floor(Math.random()*aArray.length);
+        y = y_coordinates[i]; //y is a random coordinate from 'y_coordinates' if that coordinate still exists
+        
+        switch (y) {
+
+            //middle
+            case 290:
+                x = 390;
+                break;
+        
+            //top right
+            case 115:
+                x = 905;
+                break;
+
+            //bottom right
+            case 635:
+                x = 905;
+                break;
+        
+            //bottome left
+            case 634:
+                x = 390;
+                break;
+        }
+
+        if(j == 0) {
+
+            ans[j] = new Answer(x, y, correctA);
+            aArray.splice(m, 1);
+        
+
+        } else {
+
+            ans[j] = new Answer(x, y, aArray[n]); 
+            aArray.splice(n, 1);
+        }
+        
+        j++;
+        y_coordinates.splice(i, 1); //so that we can't reuse that y coordinate
+    
+    }
+}
+
+//return correct answer to question being asked
+function correctA4(question)
+{
+    var answer;
+
+    switch (question)
+    {
+        case "What logical operator ensures that BOTH boolean expressions are true for the overall expression to be true?":
+            answer = "&&";
+            break;
+
+        case "What logical operator ensures that AT LEAST ONE boolean expression is true for the overall expression to be true?":
+            answer = "||";
+            break;
+
+        case "What loop contains the loop control variable, boolean condition, and variable updating on one line of code?":
+            answer = "for loop";
+            break;
+        
+        case "What is one type of decision structure?":
+            answer = "if-else";
+            break;
+        
+        case "What is an example of an exit-control loop that always executes at least once?":
+            answer = "do-while";
+            break;
+        
+        case "What system level function lets you format the console output using format specifiers and their corresponding arguments?":
+            answer = "printf";
+            break;
+        
+        case "In a switch case statement, if none of the case label expressions match the switch expression, which optional label is branched to instead?":
+            answer = "default";
+            break;
+        
+        case "What logical operator inverts the boolean value of a boolean expression?":
+            answer = "!";
+            break;
+
+        case "For a switch case statement, which keyword should terminate a case label expression to stop all remaining case expressions from executing?":
+            answer = "break";
+            break;
+        
+        case "Which relational operator tests for equality between its two operands?":
+            answer = "==";
+            break;
+
+    }
+
+    return answer;
+}
+
+//checks to see if the answer is correct 
+function checkAnswer4(x,y) {    
+   
+    for(i = 0; i < 4; i++) {
+        if(x == ans[i].column && y == ans[i].row) {
+            if(ans[i].answer == answer4) {
+             
+                total += 200;
+                document.getElementById("ScoreBoard").innerHTML = "Score : " + total;
+                pig.entered();
+                newRound4();
+                
+                
+                break;
+               
+            } else {
+
+                youLose();
+                total = 0;
+                document.getElementById("ScoreBoard").innerHTML = "Score : " + total;
+                pig.entered();
+                newRound4();
+                break;
+
+            }
+        }
+
+    }
+    
+
+}
+
+//remove current questions and answers when player wins the game
+function newRound4() {
+    for(k = 0; k < ans.length; k++) {
+    
+        ans[k].removeA();
+
+     }
+
+    for(h = 0; h < hay.length; h++) {
+
+        hay.pop();
+  
+    }
+
+    levelFour();
+    q.remove();
+    displayQ();
+    
+
+    
+}
+
