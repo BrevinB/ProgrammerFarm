5b295d64c2f1ba76b76b3f0c505706de0144ec93
raops01 <raops01@pfw.edu>
2022-09-17 21:28:56 -0400

    font size altered for larger answers

--- Functionality/Q&A.js
+++ Functionality/Q&A.js
@@ -17,9 +17,21 @@ class Answer {
         rect(this.column, this.row, 85, 85);
 
         fill(0);
+
+        //adjust font size if answer cannot fit in answer grid
+        if (str(this.answer).length < 9)
+        {
+          textSize(10);
+        }
+
+        else
+        {
+          textSize(8.5);
+        }
         this.p = text(this.answer, this.column+40, this.row+50);
+
         textAlign(CENTER);
-        textSize(10);
+        //textSize(9);
         textFont(arcadeFont);
         
 
